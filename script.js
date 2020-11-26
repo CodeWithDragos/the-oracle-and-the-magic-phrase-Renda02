cc78d3bf873d0a80b2b2d4293e55f64e97953522
Renda02 <rendaniluvhengo89@gmail.com>
2020-11-26 07:51:05 +0100

    Added message box

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // Use the skeleton bellow as a basis for your script
 var generateBtn = document.querySelector("#magic-btn");
+function makeSomeMagic() {
+   confirm("Do I like to do sports?");
+   confirm("Do I like to make puzzles?");
+   confirm("Do I like pets?");
+   confirm("Do I like pizza?");
+   confirm("Do I like to draw or to read?");
+   confirm("am I a morning person or a evening one");
+
+}
+
 
 function displayMagicPhrase() {
+ 
   var magicPhrase = makeSomeMagic();
+  magicPhrase = "Your future is doomed. But hey you can always try again :)";
   var magicPhraseDisplay = document.querySelector("#magic-phrase");
 
   magicPhraseDisplay.value = magicPhrase;
